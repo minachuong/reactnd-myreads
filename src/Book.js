6bedf10bb75400789bc3b361f3d73447021828ce
Mina Chuong <mina.chuong@gmail.com>
2017-08-07 13:29:12 -0700

    feat: Add prop-types package

--- src/Book.js
+++ src/Book.js
@@ -11,7 +11,7 @@ class Books extends Component {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
             <div className="book-shelf-changer">
-              <select>
+              <select value={book.shelf}>
                 <option value="none" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
@@ -22,7 +22,7 @@ class Books extends Component {
           </div>
           <div className="book-title">{book.title}</div>
           {book.authors.map((author) => (
-            <div className="book-authors">{author}</div>
+            <div className="book-authors" key={author}>{author}</div>
           ))}
         </div>
       </li> 
