1b98bfa26d7423e39b49fa4598b5d44c4fab7f3c
Mina Chuong <mina.chuong@gmail.com>
2017-08-28 13:48:28 -0700

    feat: Add check for response from BooksAPI to not update state unless response is an array

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -9,16 +9,17 @@ class SearchBooks extends Component {
     query: "",
   }
   
-  search = (query, maxResults) => {
-    BooksAPI.search(query, maxResults).then((response) => { 
-      this.setState({ 
-        availableBooks: response,
-        query: query,
-      })
-    })
-  }
-
-  componentDidMount() {
+  search = (query) => {
+    BooksAPI.search(query, 50).then((response) => { 
+      if (Array.isArray(response)) {
+        this.setState({ 
+          availableBooks: response,
+          query: query,
+        })
+      } else {
+        this.setState({ query: query })
+      }  
+    }) 
   }
 
   render() {
@@ -27,30 +28,19 @@ class SearchBooks extends Component {
         <div className="search-books-bar">
           <Link to="/" className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
-            {/* 
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-              
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
             <input 
               type="text" 
               placeholder="Search by title or author" 
               value={this.state.query} 
-              onChange={(event, maxResults) => this.search(event.target.value, 50)} 
+              onChange={(event) => this.search(event.target.value)} 
             />
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {this.state.availableBooks !== [] ? (
-              console.log(this.state)
-             // this.state.availableBooks.map((book) => (
-             //   <Book key={book.id} book={book} />
-             // ))
-            ) : ( null )}
+            {this.state.availableBooks.map((book) => (
+               <Book key={book.id} book={book} reloadShelf={() => {}} />
+            ))}
           </ol>
         </div>
       </div>
