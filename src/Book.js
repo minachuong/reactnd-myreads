21548d89aeb0b904b1f82253305c5c190fa7faca
Mina Chuong <mina.chuong@gmail.com>
2017-08-29 12:57:21 -0700

    feat: Books in the SearchBooks component have a default value of 'none'

--- src/Book.js
+++ src/Book.js
@@ -18,7 +18,7 @@ class Books extends Component {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
             <div className="book-shelf-changer">
-              <select value={book.shelf} onChange={(event) => this.updateBook(book, event.target.value)}>
+              <select value={book.shelf === undefined ? "none" : book.shelf } onChange={(event) => this.updateBook(book, event.target.value)}>
                 <option value="none" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
